0b78dc4c76f6d8460154d283595c553d2a20f64b
Gonçalo Morais <gnclmorais@gmail.com>
2022-03-21 23:39:41 +0100

    Try different way to check a command

--- scripts/03-test-tools.sh
+++ scripts/03-test-tools.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-which volta
-
-if [[ $(which volta) ]]
+if ! command -v volta &> /dev/null
 then
-  echo "Volta installation succeeded"
-  exit 0
-else
   echo "Volta installation failed" >&2
   exit 1
+else
+  echo "Volta installation succeeded"
+  exit 0
 fi
