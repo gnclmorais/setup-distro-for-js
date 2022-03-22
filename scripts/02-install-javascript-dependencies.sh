f74365407cd711801aba3ecba665d6f579f3ef6d
Gonçalo Morais <gnclmorais@gmail.com>
2022-03-22 08:42:47 +0100

    Tweak Volta setup

--- scripts/02-install-javascript-dependencies.sh
+++ scripts/02-install-javascript-dependencies.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# We need curl to easy fetch Volta
 sudo apt-get --quiet install curl
 
-curl https://get.volta.sh | bash
-
-source ~/.bashrc
+if grep -R '# Volta setup' ~/.bashrc
+then
+  echo 'Volta already installed'
+else
+  # Skip automatic setup and do it manually
+  curl https://get.volta.sh | bash -s -- --skip-setup
+  echo >> ~/.bashrc
+  echo '# Volta setup' >> ~/.bashrc
+  echo 'export VOLTA_HOME="$HOME/.volta"' >> ~/.bashrc
+  echo 'export PATH=$PATH:$VOLTA_HOME/bin' >> ~/.bashrc
+fi
