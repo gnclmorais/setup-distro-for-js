1de398a8189b5d3f946579774352a075e04744c1
Gonçalo Morais <gnclmorais@gmail.com>
2022-03-22 21:00:57 +0100

    Add git

--- scripts/01-install-general-dependencies.sh
+++ scripts/01-install-general-dependencies.sh
@@ -1,4 +1,4 @@
 # Instal updates and building tools
 sudo apt-get --quiet --assume-yes update
 sudo apt-get --quiet --assume-yes upgrade
-sudo apt-get --quiet --assume-yes install gcc perl make
+sudo apt-get --quiet --assume-yes install git gcc perl make
