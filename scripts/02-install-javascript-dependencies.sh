2a32a34046f4bf0876176a659bdb81e757da880e
Gonçalo Morais <gnclmorais@gmail.com>
2022-03-22 13:30:12 +0000

    Add assume-yes, part 2

--- scripts/02-install-javascript-dependencies.sh
+++ scripts/02-install-javascript-dependencies.sh
@@ -1,5 +1,5 @@
 # We need curl to easy fetch Volta
-sudo apt-get --quiet install curl
+sudo apt-get --quiet --assume-yes install curl
 
 # Check if Volta has been setup already
 if grep -R 'VOLTA_HOME' ~/.bashrc
