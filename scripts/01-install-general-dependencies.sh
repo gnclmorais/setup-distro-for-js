96b93d0befe2e6b0048a7ecdeaf39da750a25187
Gonçalo Morais <gnclmorais@gmail.com>
2022-03-23 13:16:09 +0000

    Add VMware tools

--- scripts/01-install-general-dependencies.sh
+++ scripts/01-install-general-dependencies.sh
@@ -4,3 +4,6 @@ sudo apt-get --quiet --assume-yes upgrade
 
 # Install common tools
 sudo apt-get --quiet --assume-yes install gcc perl make git vim
+
+# Install VM tools (for VMware Fusion)
+sudo apt-get --quiet --assume-yes install open-vm-tools-desktop
