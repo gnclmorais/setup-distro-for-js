89fa921abe973b6fa55007a40d167538d2341efd
Gonçalo Morais <gnclmorais@gmail.com>
2022-07-08 14:59:53 +0200

    Add missing addition to compiled script

--- dist/all.sh
+++ dist/all.sh
@@ -6,6 +6,9 @@ sudo apt-get --quiet --assume-yes upgrade
 # Install common tools
 sudo apt-get --quiet --assume-yes install gcc perl make git vim
 
+# Install VM tools (for VMware Fusion)
+sudo apt-get --quiet --assume-yes install open-vm-tools-desktop
+
 # We need curl to easy fetch Volta
 sudo apt-get --quiet --assume-yes install curl
 
@@ -21,6 +24,12 @@ fi
 # Install Node, npm and yarn
 volta install node
 npm install --global yarn
+
+echo
+echo 'Installed versions:'
+echo " Node: $(node --version)"
+echo "  npm: $(npm --version)"
+echo " yarn: $(yarn --version)"
 
 # Sublime Text
 sudo snap install sublime-text --classic
